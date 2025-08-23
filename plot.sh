cd6bd5cf8e7fd1577ec514f76bfbcd58d826379a
JoshuaWalter2907 <103947371+JoshuaWalter2907@users.noreply.github.com>
2025-08-23 11:45:52 +0200

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-export LC_NUMERIC=C   # Fix locale-related decimal issues
+export LC_NUMERIC=C 
 
-if [ -z "$1" ]; then
-    echo "Please enter the problem Size. Example: ./plot.sh 40000"
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <ProblemSize> <CommMethod: NCCL|NVSHMEM>"
+    echo "Example: $0 40000 NCCL"
     exit 1
 fi
 
 RUN_ID="$1"
-CSV_FILE="${RUN_ID}_N_NO_NCCL.csv"
+COMM_METHOD=$(echo "$2" | tr '[:lower:]' '[:upper:]') 
 
-INTERVAL=1  # Logging Interval                 
+if [[ "$COMM_METHOD" != "NCCL" && "$COMM_METHOD" != "NVSHMEM" ]]; then
+    echo "Error: Communication method must be NCCL or NVSHMEM."
+    exit 1
+fi
+
+CSV_FILE="${RUN_ID}_${COMM_METHOD}.csv"
+
+INTERVAL=1
 UPS_NAME="eaton-ups"
 UPS_IP="172.20.38.30"
 
@@ -27,7 +35,6 @@ FIELDS=(
     "ups.temperature"
 )
 
-# Create CSV Header if not available
 if [ ! -f "$CSV_FILE" ]; then
     {
         echo -n "timestamp"
@@ -53,11 +60,9 @@ log_data() {
     done
 }
 
-# --- 10 Seconds measurments before the Benchmark ---
 PRE_END_TIME=$(( $(date +%s) + 10 ))
 log_data "$PRE_END_TIME"
 
-# --- Edit HPL file with the entered problem size ---
 HPL_DAT_FILE="HPL-2GPUs.dat"
 if [ -f "$HPL_DAT_FILE" ]; then
     sed -i -E "s/^([[:space:]]*)[0-9]+([[:space:]]+Ns)/\1$RUN_ID\2/" "$HPL_DAT_FILE"
@@ -66,24 +71,29 @@ else
     exit 1
 fi
 
-# --- start benchmark ---
-BENCHMARK_END_FILE="/tmp/benchmark_end_$RUN_ID"
+BENCHMARK_END_FILE="/tmp/benchmark_end_${RUN_ID}_${COMM_METHOD}"
 rm -f "$BENCHMARK_END_FILE"
 
-(
-    HPL_OOC_MODE=1 mpirun -np 2 ./hpl.sh --dat "HPL-2GPUs.dat"
-    touch "$BENCHMARK_END_FILE"
-) &
+if [ "$COMM_METHOD" == "NVSHMEM" ]; then
+    (
+        HPL_OOC_MODE=1 HPL_P2P_AS_BCAST=4 HPL_USE_NVSHMEM=1 HPL_NVSHMEM_SWAP=0 \
+        mpirun -np 2 ./hpl.sh --dat "HPL-2GPUs.dat"
+        touch "$BENCHMARK_END_FILE"
+    ) &
+else
+    (
+        HPL_OOC_MODE=1 \
+        mpirun -np 2 ./hpl.sh --dat "HPL-2GPUs.dat"
+        touch "$BENCHMARK_END_FILE"
+    ) &
+fi
 
-# --- continue logging whilst Benchmark is running ---
 while [ ! -f "$BENCHMARK_END_FILE" ]; do
     INTERVAL_END=$(( $(date +%s) + INTERVAL ))
     log_data "$INTERVAL_END"
 done
 
-# --- 10 measurement after the Benchmark is finished ---
 POST_END_TIME=$(( $(date +%s) + 10 ))
 log_data "$POST_END_TIME"
 
 echo "Logging done. File: $CSV_FILE"
-
